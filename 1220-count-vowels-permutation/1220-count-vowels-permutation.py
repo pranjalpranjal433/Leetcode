1821a306b9736bf4b0886bac1c5d3d34ae639056
Pranjal Singh <110617130+pranjalpranjal433@users.noreply.github.com>
2022-08-13 21:53:44 +0530

    Time: 262 ms (81.77%), Space: 13.9 MB (85.82%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.py
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.py
@@ -22,37 +22,10 @@ class Solution:
         x=[1]*5
         mod=10**9+7
         for i in range(1,n):
-            for j in range(5):
-                if j==0:
-                    x[j]=(dp[1]+dp[2]+dp[4])%mod
-                elif j==1:
-                    x[j]=(dp[0]+dp[2])%mod
-                elif j==2:
-                    x[j]=(dp[1]+dp[3])%mod
-                elif j==3:
-                    x[j]=(dp[2])%mod
-                elif j==4:
-                    x[j]=(dp[2]+dp[3])%mod
+            x[0]=(dp[1]+dp[2]+dp[4])%mod
+            x[1]=(dp[0]+dp[2])%mod
+            x[2]=(dp[1]+dp[3])%mod
+            x[3]=(dp[2])%mod
+            x[4]=(dp[2]+dp[3])%mod
             dp=x[:]
         return (sum(dp)%mod)
-#         a = 1
-#         e = 1
-#         i = 1
-#         o = 1
-#         u = 1
-#         mod = pow(10, 9)+7;
-        
-#         for j in range(1,n):
-#             a2 = (e + i + u) % mod;
-#             e2 = (a + i) % mod;
-#             i2 = (e + o) % mod;
-#             o2 = i;
-#             u2 = (o + i) % mod;
-#             a = a2
-#             e = e2
-#             i = i2
-#             o = o2
-#             u = u2;
-#             print(a,e,i,o,u)
-        
-#         return (a + e + i + o + u) % mod;
