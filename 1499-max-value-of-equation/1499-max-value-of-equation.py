9e42086d04ffb04d1b02448a40ded4ad96987263
Pranjal Singh <110617130+pranjalpranjal433@users.noreply.github.com>
2022-08-17 15:02:23 +0530

    Time: 2088 ms (40.42%), Space: 60.6 MB (24.74%) - LeetHub

--- 1499-max-value-of-equation/1499-max-value-of-equation.py
+++ 1499-max-value-of-equation/1499-max-value-of-equation.py
@@ -2,7 +2,7 @@ from heapq import *
 class Solution:
     def findMaxValueOfEquation(self, points: List[List[int]], k: int) -> int:
         heap=[]
-        ans=-(2**31)
+        ans=-inf
         for i in points:
             while heap and abs(i[0]-heap[0][1])>k:
                 heappop(heap)
