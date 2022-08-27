a6699a4c2817929ffcfab7e7e68719f34a207f71
Pranjal Singh <110617130+pranjalpranjal433@users.noreply.github.com>
2022-08-27 14:21:01 +0530

    Time: 36 ms (84.46%), Space: 15.1 MB (5.28%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.py
@@ -0,0 +1,21 @@
+import math
+class Solution:
+    def divisorGame(self, n: int) -> bool:
+        dp=[-1]*1001
+        dp[1]=0
+        def find(n):
+            if dp[n]!=-1:
+                return dp[n]
+            p=math.sqrt(n)
+            p=((int)(p))+1
+            for i in range(1,p):
+                if (n%i==0):
+                    if find(n-i)==0:
+                        dp[n]=1
+                        return 1
+                    if i!=1 and find(n-(n//i))==0:
+                        dp[n]=1
+                        return 1
+            dp[n]=0
+            return 0
+        return find(n)
