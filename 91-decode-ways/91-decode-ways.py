59caa88cc7ed9b3781a0308ac18d15da2f649aec
Pranjal Singh <110617130+pranjalpranjal433@users.noreply.github.com>
2022-08-27 22:35:07 +0530

    Time: 34 ms (92.30%), Space: 14.3 MB (6.50%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -7,15 +7,25 @@ class Solution:
             if num in d:
                 return True
             return False
+        c=[0]
         l=len(s)
-        dp=[0]*(l+1)
-        dp[0]=1
-        dp[1]=1 if isVal(s[0]) else 0
-        for i in range(2,l+1):
-            if isVal(s[i-1]):
-                dp[i]+=dp[i-1]
-            if isVal((s[i-2]+s[i-1])):
-                dp[i]+=dp[i-2]
-            if dp[i]==0:
-                break
-        return dp[l]
+        dp=[-1]*(l+1)
+        dp[l]=1
+        def find(ind):
+            if ind==l:
+                return dp[l]
+            if dp[ind]!=-1:
+                return dp[ind]
+            one = s[ind]
+            ans=0
+            if isVal(one):
+                ans=find(ind+1)
+            if ind<(l-1):
+                two = s[ind]+s[ind+1]
+                if isVal(two):
+                        ans+=find(ind+2)
+            dp[ind]=ans
+            return dp[ind]
+        dp[0]=find(0)
+        dp[0]=dp[0] if dp[0]>-1 else 0
+        return dp[0]
