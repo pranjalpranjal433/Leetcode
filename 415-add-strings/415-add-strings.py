993a48987d8ab8cf37fadd7f4eb76ea3c6c75787
Pranjal Singh <110617130+pranjalpranjal433@users.noreply.github.com>
2022-09-30 23:08:45 +0530

    Time: 73 ms (43.59%), Space: 13.9 MB (88.51%) - LeetHub

---
+++ 415-add-strings/415-add-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addStrings(self, num1: str, num2: str) -> str:
+        # num1=num1[::-1]
+        # num2=num2[::-1]
+        # num3='0'
+        # l1
+        a=int(num1)
+        b=int(num2)
+        return str(a+b)
